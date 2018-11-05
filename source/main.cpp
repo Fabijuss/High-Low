8e3c136afb81522bd641dfdf115976c68c64e05a
Fabijus Sobolevas <43203019+Fabijuss@users.noreply.github.com>
2018-11-05 08:23:45 +0000

    Version 1.3

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,6 @@
 /* A simple game where the player must guess if their roll is higher or lower than the computer's roll. */
 /* By Fabijus Sobolevas */
-/* Version 1.2 */ 
+/* Version 1.3 */ 
 #include <time.h> /* time */
 #include <stdlib.h> /* srand, rand  */
 #include <iostream>
@@ -17,12 +17,14 @@ int main()
   /* Generates a random number between 1-10 using time seed */
   cPick = rand() % 10 + 1;
   pPick = rand() % 10 + 1;
-  
-    cout << "Higher or Lower?" << endl;
+
+    cout << "Higher or Lower? (higher/lower)" << endl;
 
     getline (cin,choice);
 
-  /* Asks for choice input until it's either "lower" or "higher" */
+/* Asks for input until choice is either "lower" or "higher"  */
+/* Preconditions: Input must equal either "lower" or "higher" */
+/* Postconditions: Will ask for input if choice is invalid */
   if (choice != "higher" && choice != "lower") 
   {
   do {
@@ -32,10 +34,10 @@ int main()
   while (choice != "higher" && choice != "lower");
   }
 
-  /*  */
-  if (cPick > pPick && choice == "higher"  || cPick < pPick && choice == "lower" )
+  /* Decide winner */
+  if ((cPick > pPick && choice == "higher") || (cPick < pPick && choice == "lower"))
     cout << "You lose!" << endl;
-  else if (cPick < pPick && choice =="higher" || cPick > pPick && choice =="lower")
+  else if ((cPick < pPick && choice =="higher") || (cPick > pPick && choice =="lower"))
      cout << "You win!" << endl;
   else
     cout << "It's a tie!" << endl;
@@ -43,17 +45,25 @@ int main()
   cout << "The computer rolled: " << cPick << endl;
   cout << "The player rolled: " << pPick << endl;
 
-  cout << "Try again? Y/N " << endl;
-  
+  /* Restart program */
+  cout << "Try again? (y/n) " << endl;
+
   getline(cin,restart);
 
- if (choice != "y" && choice != "n") 
+  if (restart != "y" && restart != "n") 
   {
   do {
       cout << "Invalid choice, try again." << endl;
-      getline (cin,choice);
+      getline (cin,restart);
   } 
-  while (choice != "y" && choice != "n");
+  
+  while (restart != "y" && restart != "n");
   }
-}
 
+   if (restart == "y") 
+  {
+      main();
+  }
+  
+  else {}
+}
