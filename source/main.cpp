23c696965bcaf12053c2ea8c234a067f30866313
Fabijus Sobolevas <43203019+Fabijuss@users.noreply.github.com>
2018-11-05 08:04:39 +0000

    Version 1.2

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,6 @@
 /* A simple game where the player must guess if their roll is higher or lower than the computer's roll. */
 /* By Fabijus Sobolevas */
-/* Version 1.0 */ 
+/* Version 1.2 */ 
 #include <time.h> /* time */
 #include <stdlib.h> /* srand, rand  */
 #include <iostream>
@@ -10,19 +10,50 @@ int main()
 { 
   int cPick;
   int pPick;
+  string choice;
+  string restart;
   /* Generates random seed using the current time */
   srand(time(NULL));
   /* Generates a random number between 1-10 using time seed */
   cPick = rand() % 10 + 1;
   pPick = rand() % 10 + 1;
   
-  if (cPick > pPick)
-    cout << "Computer wins!" << endl;
-  else if (cPick < pPick)
-     cout << "Player wins!" << endl;
+    cout << "Higher or Lower?" << endl;
+
+    getline (cin,choice);
+
+  /* Asks for choice input until it's either "lower" or "higher" */
+  if (choice != "higher" && choice != "lower") 
+  {
+  do {
+      cout << "Invalid choice, try again." << endl;
+      getline (cin,choice);
+  } 
+  while (choice != "higher" && choice != "lower");
+  }
+
+  /*  */
+  if (cPick > pPick && choice == "higher"  || cPick < pPick && choice == "lower" )
+    cout << "You lose!" << endl;
+  else if (cPick < pPick && choice =="higher" || cPick > pPick && choice =="lower")
+     cout << "You win!" << endl;
   else
     cout << "It's a tie!" << endl;
 
   cout << "The computer rolled: " << cPick << endl;
   cout << "The player rolled: " << pPick << endl;
+
+  cout << "Try again? Y/N " << endl;
+  
+  getline(cin,restart);
+
+ if (choice != "y" && choice != "n") 
+  {
+  do {
+      cout << "Invalid choice, try again." << endl;
+      getline (cin,choice);
+  } 
+  while (choice != "y" && choice != "n");
+  }
 }
+
